d4ebcb56d45928fda803bbf7f12187fba7645ff9
Juan Simancas <dev.juansimancas@proton.me>
2024-09-06 22:38:08 -0500

    added Catppuccin Latte and Moccha

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,26 @@
 export default {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        base: {
+          light: '#eff1f5',
+          dark: '#1e1e2e'
+        },
+        text: {
+          light: '#4c4f69',
+          dark: '#cdd6f4',
+        },
+        crust: {
+          light: '#dce0e8',
+          dark: '#11111b',
+        },
+        accent: {
+          light: '#7287fd',
+          dark: '#b4befe',
+        },
+      }
+    },
   },
   plugins: [],
 }
