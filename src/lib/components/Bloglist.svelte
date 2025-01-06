750cd3ac6b13d478bf35904efc22b9425ef83fa5
Juan Simancas <dev.juansimancas@proton.me>
2025-01-05 20:42:01 -0500

    update how bloglist is displayed

--- src/lib/components/Bloglist.svelte
+++ src/lib/components/Bloglist.svelte
@@ -2,15 +2,12 @@
     export let blogData;
 </script>
 
-<div class="border-2 p-5 rounded-none">
-    <div class="flex flex-row space-x-2 items-baseline">
-        <a href={blogData.path}>
-            <h2 class="font-semibold text-2xl hover:text-accent-light">
-                {blogData.title}
-            </h2>
-        </a>
-        <span>⦁</span>
-        <p class="text-base font-light italic">{blogData.date}</p>
-    </div>
-    <p>{blogData.description}</p>
+<div class="flex flex-row items-baseline space-x-2">
+    <p class="font-light w-20">{blogData.date}</p>
+    <a
+        href={blogData.path}
+        class="font-semibold flex-1 hover:text-accent-light"
+    >
+        {blogData.title}
+    </a>
 </div>
