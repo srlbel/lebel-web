9c2a926a3f1cee8620f9d87f6fb89f1449f09b5d
Juan Simancas <dev.juansimancas@proton.me>
2025-01-05 19:11:44 -0500

    add filter to only show published blogs

--- src/routes/api/posts/+server.js
+++ src/routes/api/posts/+server.js
@@ -4,7 +4,9 @@ import { json } from '@sveltejs/kit';
 export const GET = async () => {
     const allPosts = await fetchMarkdownPosts();
 
-    const sortedPosts = allPosts.sort((a, b) => {
+    const filteredPosts = allPosts.filter(post => post.meta.published === true)
+
+    const sortedPosts = filteredPosts.sort((a, b) => {
         return new Date(b.meta.date) - new Date(a.meta.date);
     });
 
