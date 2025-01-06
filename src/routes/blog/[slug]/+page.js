23f05c95e86d55412d180c781e42c1504234dd86
Juan Simancas <dev.juansimancas@proton.me>
2025-01-05 20:54:00 -0500

    update how blog metadata is displayed

--- src/routes/blog/[slug]/+page.js
+++ src/routes/blog/[slug]/+page.js
@@ -1,11 +1,12 @@
 export const load = async ({ params }) => {
     const post = await import(`../${params.slug}.md`)
-    const { title, date } = post.metadata
+    const { title, date, description } = post.metadata
     const content = post.default
 
     return {
         content,
         title,
-        date
+        date,
+        description
     }
 }
--- src/routes/blog/[slug]/+page.svelte
+++ src/routes/blog/[slug]/+page.svelte
@@ -2,8 +2,11 @@
     export let data;
 </script>
 
-<h1>{data.title}</h1>
-<p>Published: {data.date}</p>
+<div class="items-center flex flex-col my-10">
+    <h1 class="text-3xl font-bold text-center">{data.title}</h1>
+    <p class="text-sm text-center">{data.date}</p>
+    <p class="p-2 text-center">{data.description}</p>
+</div>
 <div class="prose" id="blog-content">
     <svelte:component this={data.content} />
 </div>
