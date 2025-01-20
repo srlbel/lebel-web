59c5b551869fcb648a4c2cd0571c7043ab93d70f
Juan Simancas <dev.juansimancas@proton.me>
2025-01-19 21:32:58 -0500

    fix image aspect ratio

--- src/lib/components/skillcard.svelte
+++ src/lib/components/skillcard.svelte
@@ -5,6 +5,8 @@
 <div
   class="flex flex-col border-2 rounded-md p-3 justify-center items-center min-w-28 border-surface-light dark:border-surface-dark hover:bg-surface-light hover:dark:bg-surface-dark transition-all"
 >
-  {@render children?.()}
+  <div class="aspect-square w-12 h-12">
+    {@render children?.()}
+  </div>
   <p>{name}</p>
 </div>
