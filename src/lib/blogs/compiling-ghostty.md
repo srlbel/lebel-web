4fccc3693fd12f81531f67152db735ad964c8af4
Juan Simancas <dev.juansimancas@proton.me>
2025-01-28 12:19:53 -0500

    run prettier

--- src/lib/blogs/compiling-ghostty.md
+++ src/lib/blogs/compiling-ghostty.md
@@ -2,7 +2,7 @@
 title: Compiling Ghostty in Void Linux
 date: 28-12-2024
 description: A quick guide on how to compile the new terminal Ghostty in Void Linux
-tags: ['terminal', 'linux']
+tags: ["terminal", "linux"]
 published: true
 ---
 
@@ -11,6 +11,7 @@ published: true
 By the time I made wrote this, [ghostty](https://ghostty.org/) haven't announced a way to compile Ghostty in Void Linux and neither is available in the [Void Packages](https://github.com/void-linux/void-packages) repo. So, there's probably a better way to install Ghostty using **xbps** by the time I publish this.
 
 # Prerequisites
+
 In order to compile you need the following packages:
 
 - zig
@@ -19,7 +20,7 @@ In order to compile you need the following packages:
 - pkg-config
 - git
 
-**gtk4-devel**, **libadwaita-devel** and **zig** are required for building ghostty's UI. **pkg-config** is mostly required to link the files from the dependencies to build the codebase. 
+**gtk4-devel**, **libadwaita-devel** and **zig** are required for building ghostty's UI. **pkg-config** is mostly required to link the files from the dependencies to build the codebase.
 
 # Process
 
--- svelte.config.js
+++ svelte.config.js
@@ -1,39 +1,38 @@
-import adapter from '@sveltejs/adapter-auto';
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
-import { mdsvex, escapeSvelte } from 'mdsvex';
-import rehypeSlug from 'rehype-slug';
-import remarkToc from 'remark-toc';
-import { createHighlighter } from 'shiki'
+import adapter from "@sveltejs/adapter-auto";
+import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
+import { mdsvex, escapeSvelte } from "mdsvex";
+import rehypeSlug from "rehype-slug";
+import remarkToc from "remark-toc";
+import { createHighlighter } from "shiki";
 
 /** @type {import('mdsvex').MdsvexOptions} */
 const mdsvexConfig = {
-	extensions: ['.md'],
-	remarkPlugins: [[remarkToc, { tight: true }]],
-	rehypePlugins: [rehypeSlug],
-	highlight: {
-		highlighter: async (code, lang = 'text') => {
-			const highlighter = await createHighlighter({
-				themes: ['catppuccin-latte', 'catppuccin-mocha'],
-				langs: ['javascript', 'typescript', 'bash', 'shell']
-			})
-			await highlighter.loadLanguage('javascript', 'typescript')
-			const html = escapeSvelte(highlighter.codeToHtml(code, { lang, theme: 'catppuccin-latte' }))
-			return `{@html \`${html}\` }`
-		}
-	}
-}
+  extensions: [".md"],
+  remarkPlugins: [[remarkToc, { tight: true }]],
+  rehypePlugins: [rehypeSlug],
+  highlight: {
+    highlighter: async (code, lang = "text") => {
+      const highlighter = await createHighlighter({
+        themes: ["catppuccin-latte", "catppuccin-mocha"],
+        langs: ["javascript", "typescript", "bash", "shell"],
+      });
+      await highlighter.loadLanguage("javascript", "typescript");
+      const html = escapeSvelte(
+        highlighter.codeToHtml(code, { lang, theme: "catppuccin-latte" }),
+      );
+      return `{@html \`${html}\` }`;
+    },
+  },
+};
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	extensions: ['.svelte', '.md'],
-	preprocess: [
-		vitePreprocess(),
-		mdsvex(mdsvexConfig)
-	],
+  extensions: [".svelte", ".md"],
+  preprocess: [vitePreprocess(), mdsvex(mdsvexConfig)],
 
-	kit: {
-		adapter: adapter()
-	}
+  kit: {
+    adapter: adapter(),
+  },
 };
 
 export default config;
