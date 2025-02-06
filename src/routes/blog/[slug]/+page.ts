9b411ca98cd32d6cb7fa12d9f6cb21a2bdb5b6a7
Juan Simancas <dev.juansimancas@proton.me>
2025-02-06 16:08:05 -0500

    fix non published blogs are not showed anymore

--- src/routes/blog/[slug]/+page.ts
+++ src/routes/blog/[slug]/+page.ts
@@ -5,6 +5,8 @@ export const load: PageLoad = async ({ params }) => {
 	try {
 		const post = await import(`$lib/blogs/${params.slug}.md`);
 
+		if (!post.metadata.published) throw error(404, `Could not fund ${params.slug}`);
+
 		return {
 			content: post.default,
 			meta: post.metadata
