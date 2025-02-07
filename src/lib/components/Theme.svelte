fb0e32455fca62864f01e30ee63f9012a154d812
Juan Simancas <dev.juansimancas@proton.me>
2025-02-07 08:26:08 -0500

    fix FOUC

--- src/lib/components/Theme.svelte
+++ src/lib/components/Theme.svelte
@@ -1,38 +1,17 @@
 <script lang="ts">
 	import { Moon, Sun } from 'lucide-svelte';
-	import { browser } from '$app/environment';
-
 	let darkMode = $state(false);
 
 	const handleToggle = () => {
 		darkMode = !darkMode;
 		localStorage.setItem('theme', darkMode ? 'dark' : 'light');
 
-		const blogContent = document.getElementById('blog-content');
 		if (darkMode) {
-			blogContent?.classList.add('prose-dark');
 			document.documentElement.classList.add('dark');
 		} else {
-			blogContent?.classList.remove('prose-dark');
 			document.documentElement.classList.remove('dark');
 		}
 	};
-
-	if (browser) {
-		const blogContent = document.getElementById('blog-content');
-		if (
-			localStorage.theme === 'dark' ||
-			(!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)
-		) {
-			blogContent?.classList.add('prose-dark');
-			document.documentElement.classList.add('dark');
-			darkMode = true;
-		} else {
-			blogContent?.classList.remove('prose-dark');
-			document.documentElement.classList.remove('dark');
-			darkMode = false;
-		}
-	}
 </script>
 
 <button onclick={handleToggle} class="px-2" aria-label="Toggle Theme">
